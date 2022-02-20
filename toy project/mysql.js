516eec91711fff1d803a0e0cd2644d86255f88f1
Hyebin Yu <77913313+hyebinyu1110@users.noreply.github.com>
2022-02-20 22:17:25 +0900

    Add files via upload

---
+++ toy project/mysql.js
@@ -0,0 +1,20 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'hby6362488',
+  database : 'dotconnector'
+});
+ 
+connection.connect();
+ 
+connection.query('SELECT * FROM topic', function (error, results, fields) {
+  if (error) {
+    console.log(error);
+
+  }   
+  
+  console.log(results);
+});
+ 
+connection.end();
