471bf7eb504defc83af8e36eb3cbe129124d031f
Hyebin Yu <77913313+hyebinyu1110@users.noreply.github.com>
2022-03-30 11:10:16 +0900

    Update auth.js

--- session_passport.js/lib/auth.js
+++ session_passport.js/lib/auth.js
@@ -11,7 +11,8 @@ module.exports = {
   statusUI:function(request, response){
     var authStatusUI = `<a href="/auth/login">login</a>`
     if(this.isOwner(request, response)){
-      authStatusUI = `${request.user.nickname} | <a href="/auth/logout">logout</a> `; // isOwner의  값을 구현했기 때문에, create/update/delete 같은것으로 접근해도 소유자가 아니면 접근할 수 없도록 이미 처리가 되어있으니 확인 해 볼것
+      authStatusUI = `${request.user.nickname} | <a href="/auth/logout">logout</a> `; 
+      // isOwner의  값을 구현했기 때문에, create/update/delete 같은것으로 접근해도 소유자가 아니면 접근할 수 없도록 이미 처리가 되어있으니 확인 해 볼것
     }
     return authStatusUI;
   }
