2d1f4f0ae753c04a98bbeb6c645b0e043fc44d51
Hyebin Yu <77913313+hyebinyu1110@users.noreply.github.com>
2022-04-21 17:10:33 +0900

    Update TOC.js

--- react/src/components/TOC.js
+++ react/src/components/TOC.js
@@ -1,23 +1,35 @@
 import React, { Component } from 'react';
+// 리액트라고 하는 라이브러리에서 component라고 하는 클래스를 로딩한 것이다. 
 
-class TOC extends Component{
+class TOC extends Component {
+    
     render(){
-      var lists = [];
-      var data = this.props.data;
-      var i = 0;
-      while(i < data.length){
-        lists.push(<li key={data[i].id}><a href={"/content/"+data[i].id}>{data[i].title}</a></li>);
-        i = i + 1;
-      }
+        console.log('TOC render');
+        var lists = [];
+        var data  = this.props.data;
+        var i = 0;
+        while(i < data.length){
+            lists.push(
+            <li key={data[i].id}>
+              <a 
+                  href={"/contents/"+data[i].id}
+                  data-id={data[i].id}
+                  onClick={function(e){
+                    e.preventDefault();
+                    this.props.onChangePage(e.target.dataset.id);
+                  }.bind(this)}
+                  >{data[i].title}</a>
+            </li>)
+            i += 1;
+        }
       return (
         <nav>
-            <ul>
-                {lists}
-            </ul>
-        </nav>
+          <ul>
+              {lists}
+          </ul>
+      </nav>
       );
     }
-    }
+  }
 
-
-    export default TOC;
+  export default TOC;
