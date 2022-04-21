b782e472d870b52b35a7f215c5215228ac055e99
Hyebin Yu <77913313+hyebinyu1110@users.noreply.github.com>
2022-04-21 17:11:24 +0900

    Update Content.js

--- react/src/components/Content.js
+++ react/src/components/Content.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 
-class ReadContent extends Component{
+class Content extends Component {
     render(){
-      console.log('Content render');
+        console.log('Content render');
       return (
         <article>
-            <h2>{this.props.title}</h2>
-            {this.props.desc}
-        </article>
+          <h2>{this.props.title}</h2>
+          {this.props.desc}
+      </article>
       );
     }
   }
-
-export default ReadContent;
+  
+  export default Content;
+  
