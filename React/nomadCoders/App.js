92b10c399cc90e3b5c7acd716f9f0118d7084d52
Hyebin Yu <77913313+hyebinyu1110@users.noreply.github.com>
2022-05-10 22:31:43 +0900

    Update App.js

--- React/nomadCoders/App.js
+++ React/nomadCoders/App.js
@@ -1,38 +1,24 @@
 
 import { useState, useEffect } from "react";
 
+function Hello(){
+  useEffect(function(){
+    console.log("created :0");
+    return function(){
+      console.log("bye :(");
+    }
+  }, [])
+  return <h1>Hellow</h1>
+}
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
+  const [showing, setShowing] = useState(true);
+  const onClick = () => setShowing((prev)=> !prev);
 
-
-  useEffect(() => {
-    console.log("CALL THE API...")
-  }, []);
-
-  useEffect(() => {
-    console.log("I run only when keyword changes");
-  }, [keyword]);
-
-  useEffect(() => {
-    console.log("I run only when counter changes");
-  }, [counter]);
-
-  useEffect(() => {
-    console.log("I run only when counter&counter changes");
-  }, [counter, keyword]);
 
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange} 
-        type="text" 
-        placeholder="Search here...."></input>
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      {showing? <Hello />: null}
+      <button onClick={onClick} >{showing? "HIDE" : "SHOW"}</button>
     </div>
   );
 }
