637ed259b8894b980d89174d38884d00d87b2255
Hyebin Yu <77913313+hyebinyu1110@users.noreply.github.com>
2022-05-10 16:55:38 +0900

    Create App.js

---
+++ React/nomadCoders/App.js
@@ -0,0 +1,23 @@
+
+
+import styles from "./App.module.css"
+
+import {useState, useEffect} from "react";
+
+function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev +1);
+  console.log("i run all the time");
+
+useEffect( () => {
+  console.log("i run only once")
+}, []);
+  return (
+    <div>
+          <h1>{counter}</h1>
+          <button onClick={onClick}>click me</button>
+    </div>
+  );
+}
+
+export default App;
