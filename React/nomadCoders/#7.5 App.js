13d4cd912f13c6fd54300cc63d50bcef50cca795
Hyebin Yu <77913313+hyebinyu1110@users.noreply.github.com>
2022-05-13 06:00:59 +0900

    Create #7.5 App.js

---
+++ React/nomadCoders/#7.5 App.js
@@ -0,0 +1,26 @@
+
+import { useEffect, useState } from "react";
+import Movie from "./components/Movie";
+import Detail from "./components/Details";
+import { // url 을 바라보고 있다가 url이 바뀌면 어떤걸 보여줄지 결정함 
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  // 브라우저 새로고침 없어도 유저를 다른 페이지로 이동시켜주는 컴포넌트 
+} from "react-router-dom"; // 스크린을 route 별로 생각하기
+import Home from "./route/Home"
+
+function App() {
+
+  return <Router> 
+    <Switch>
+      <Route path="/movie/:id">
+        <Detail />
+      </Route>
+      <Route path="/">
+        <Home />
+      </Route>
+    </Switch>
+  </Router>
+}
+export default App;
