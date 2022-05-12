673ba28c49f24dd0ee33f16082e01ef593600bc9
Hyebin Yu <77913313+hyebinyu1110@users.noreply.github.com>
2022-05-12 21:51:36 +0900

    Create App.js

---
+++ React/nomadCoders/#7.4/App.js
@@ -0,0 +1,10 @@
+
+import { useEffect, useState } from "react";
+import Movie from "./components/Movie";
+import Detail from "./components/Details";
+
+function App() {
+
+  return null;
+}
+export default App;
